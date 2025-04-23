0ba91f60454ef23e56bd0d262d28accc16d712af
J1ggy2B <watsongraeme418@mail.uk>
2025-04-23 12:11:34 +0100

    initial job structure

---
+++ src/main/java/com/mypackage/Program.java
@@ -0,0 +1,6 @@
+package com.mypackage; 
+public class Program { 
+public static void main(String[] args) { 
+System.out.println("This program is a Maven project and the package is com.package"); 
+} 
+} 
